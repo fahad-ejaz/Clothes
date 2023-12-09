3a9225d4ab72e52d734335c6462117afcd1bafa7
Fahad Ejaz <fahad_ejaz10@hotmail.com>
2023-12-08 23:56:08 -0500

    set up gh pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import { HashRouter } from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
